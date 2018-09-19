f318e830e44b875990301ab7029adb77a004ea53
jaimevs20 <jaimevalverde.jv@gmail.com>
2018-09-19 11:46:01 -0300

    Ativar Ambiente

--- app/Http/Controllers/AmbienteController.php
+++ app/Http/Controllers/AmbienteController.php
@@ -31,7 +31,7 @@ class AmbienteController extends Controller
             $status = 'Ativo';
         else
             $status = 'Inativo';
-
+        
     	$dados = 'data-local="'.$ambiente->local->nome.
         '" data-tipo="'.$ambiente->tipo.
         '" data-descricao="'.$ambiente->descricao.
@@ -44,12 +44,13 @@ class AmbienteController extends Controller
 
         $btnExcluir = ' <a data-id="'.$ambiente->id.'" class="btn btn-danger btnExcluir" title="Desativar" data-toggle="tooltip"><i class="fa fa-trash-o"></i></a>';
         
-        if(!$ambiente->status){
-                $btnAtivar = ' <a data-id="'.$ambiente->id.'" class="btn btn-warning btnAtivar" '. $dados .' title="Ativar Usúário" data-toggle="tooltip" ><i class="fa fa-user-plus"> </i></a>';
-                return $btnVisualizar.$btnEditar.$btnAtivar;
-            }else{
-                return $btnVisualizar.$btnEditar.$btnExcluir;
-            }
+        if(!$ambiente->status){ //Ambiente Desativado
+            $btnAtivar = ' <a data-id="'.$ambiente->id.'" class="btn btn-warning btnAtivar" '. $dados .' title="Ativar" data-toggle="tooltip" ><i class="fa fa-plus"> </i></a>';
+            return $btnVisualizar.$btnEditar.$btnAtivar;
+        }
+        else{
+            return $btnVisualizar.$btnEditar.$btnExcluir;
+        }
     }
 
     public function store(Request $request)
@@ -98,14 +99,14 @@ class AmbienteController extends Controller
         ->editColumn('acao',function($ambiente){
             return $this->setDataButtons($ambiente);
         })
-        ->editColumn('fk_local', function($ambiente){
-            return $ambiente->local->nome;
-        })
         ->editColumn('status', function($ambiente){
             if($ambiente->status)
                 return " <span class='label label-success' style='font-size:14px'>Ativo</span>";
             else
                 return " <span class='label label-default' style='font-size:14px'>Inativo</span>";
+        })
+        ->editColumn('fk_local', function($ambiente){
+            return $ambiente->local->nome;
         })
         ->escapeColumns([0])
         ->make(true);
@@ -141,7 +142,7 @@ class AmbienteController extends Controller
             $ambiente->descricao = $request->descricao;
             $ambiente->fk_local = $request->id_local;
             $ambiente->numero_ambiente = $request->numero_ambiente;
-            $ambiente->status = 'Ativo';
+         // $ambiente->status = 'Ativo';
             $ambiente->save();
 
             $ambiente->setAttribute('buttons',$this->setDataButtons($ambiente));
@@ -151,6 +152,7 @@ class AmbienteController extends Controller
 
     public function destroy($request)
     {
+        //
         $ambiente = Ambiente::find($request->id);
         $ambiente->status = 'Inativo';
         $ambiente->save();
@@ -160,5 +162,9 @@ class AmbienteController extends Controller
     public function ativar(Request $request)
     {
         //
+        $ambiente = Ambiente::find($request->id);
+        $ambiente->status = 'Ativo';
+        $ambiente->save();
+        return response()->json($ambiente);
     }
 }
